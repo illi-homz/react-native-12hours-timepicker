fd1ffa570a8033ac0aaef26b0d8a11c3635c4806
illi-homz <gopkabel@mail.ru>
2023-03-04 13:00:04 +0300

    fix fonts

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,11 +1,5 @@
 export const fonts = {
-	beVietnam900: 'BeVietnamPro-Black',
-	beVietnam800: 'BeVietnamPro-ExtraBold',
 	beVietnam700: 'BeVietnamPro-Bold',
 	beVietnam600: 'BeVietnamPro-SemiBold',
 	beVietnam500: 'BeVietnamPro-Medium',
-	beVietnam400: 'BeVietnamPro-Regular',
-	beVietnam300: 'BeVietnamPro-Light',
-	beVietnam200: 'BeVietnamPro-ExtraLight',
-	beVietnam100: 'BeVietnamPro-Thin',
 };
